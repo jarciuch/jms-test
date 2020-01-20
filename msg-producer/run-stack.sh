35483314bce4dd0f33539f5120cfb859d380dbaa
Jan Arciuchiewicz <jarciuch@gmail.com>
2020-01-20 17:57:02 +0000

    Make it work as stack

--- msg-producer/run-stack.sh
+++ msg-producer/run-stack.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export REGISTRY_URL=localhost:5000
+export MSG_CONSUMER_VERSION=0.0.1-SNAPSHOT
+export AMQ_BROKER_HOST=activemq:61616
+
 docker stack deploy --compose-file docker-compose.yml amq
 
 #To remove: docker stack rm amq
